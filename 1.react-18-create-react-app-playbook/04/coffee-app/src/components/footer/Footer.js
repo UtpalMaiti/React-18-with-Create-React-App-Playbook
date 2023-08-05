b076bceee76c9e606f9e087c542c2cc3a7f0dd12
UtpalMaiti <Utpalmaiti12@gmail.com>
2023-08-05 23:45:54 +0530

    tt

---
+++ 1.react-18-create-react-app-playbook/04/coffee-app/src/components/footer/Footer.js
@@ -0,0 +1,11 @@
+import styles from './Footer.module.css';
+
+function Footer() {
+	return (
+		<footer className={styles['app-footer']}>
+			Wired Brain Coffee Copyright 2022!
+		</footer>
+	)
+}
+
+export default Footer;
